65313582db624628238817ded4758ba893b8b4e3
Vitaliy14122018 <45883338+Vitaliy14122018@users.noreply.github.com>
2019-12-14 19:28:26 +0200

    Commerce

---
+++ OpenServer/domains/okay/compiled/okay_shop/23c0cdca2e80a9b62a559440bc9a0029c2d85038.file.cart_informer.tpl.php
@@ -0,0 +1,52 @@
+<?php /* Smarty version Smarty-3.1.19-dev, created on 2019-12-13 22:43:21
+         compiled from "C:\OpenServer\domains\okay\design\okay_shop\html\cart_informer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:20395533605df3e9d9991092-50788685%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '23c0cdca2e80a9b62a559440bc9a0029c2d85038' => 
+    array (
+      0 => 'C:\\OpenServer\\domains\\okay\\design\\okay_shop\\html\\cart_informer.tpl',
+      1 => 1576266054,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '20395533605df3e9d9991092-50788685',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'cart' => 0,
+    'lang_link' => 0,
+    'lang' => 0,
+    'currency' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.19-dev',
+  'unifunc' => 'content_5df3e9d9f2b5c4_24699185',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5df3e9d9f2b5c4_24699185')) {function content_5df3e9d9f2b5c4_24699185($_smarty_tpl) {?>
+<?php if ($_smarty_tpl->tpl_vars['cart']->value->total_products>0) {?>
+    <a href="<?php echo $_smarty_tpl->tpl_vars['lang_link']->value;?>
+cart" class="cart_info">
+        <span class="cart_counter"><?php echo $_smarty_tpl->tpl_vars['cart']->value->total_products;?>
+</span>
+        <span class="cart_title" data-language="index_cart"><?php echo $_smarty_tpl->tpl_vars['lang']->value->index_cart;?>
+</span>
+         <span class="cart_total"><?php echo $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_MODIFIER]['convert'][0][0]->convert(($_smarty_tpl->tpl_vars['cart']->value->total_price));?>
+ <?php echo htmlspecialchars($_smarty_tpl->tpl_vars['currency']->value->sign, ENT_QUOTES, 'UTF-8', true);?>
+</span>
+    </a>
+<?php } else { ?>
+    <div class="cart_info">
+        <span class="cart_counter"><?php echo $_smarty_tpl->tpl_vars['cart']->value->total_products;?>
+</span>
+        <span class="cart_title" data-language="index_cart"><?php echo $_smarty_tpl->tpl_vars['lang']->value->index_cart;?>
+</span>
+        <span class="cart_total"><?php echo $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_MODIFIER]['convert'][0][0]->convert(($_smarty_tpl->tpl_vars['cart']->value->total_price));?>
+ <?php echo htmlspecialchars($_smarty_tpl->tpl_vars['currency']->value->sign, ENT_QUOTES, 'UTF-8', true);?>
+</span>
+    </div>
+<?php }?>
+<?php }} ?>
